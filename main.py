f59547a7a6a62c29ec313db46c2f0facac11a39c
Daniel Haase <git@dhaase.de>
2020-12-17 17:33:46 +0100

    Rewrite main file

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 
+import sys
 import unittest
+
+import cv2
+import numpy as np
 
 
 class TestCase(unittest.TestCase):
-    def test_imports(self):
-        import cv2
-        print("test_imports:  OpenCV  {}".format(cv2.__version__))
+    def test_versions(self):
+        versions = get_versions()
+        self.assertIsInstance(versions, dict)
 
-        import numpy as np
-        print("test_imports:  NumPy   {}".format(np.__version__))
-        
-        self.assertTrue(True)
+
+def get_versions():
+    versions = {}
+    versions["Python"] = ".".join(str(part) for part in sys.version_info[:3])
+    versions["OpenCV"] = cv2.__version__
+    versions["NumPy"] = np.__version__
+    print("VERSIONS: {}".format(versions))
+    return versions
 
 
 if __name__ == "__main__":
